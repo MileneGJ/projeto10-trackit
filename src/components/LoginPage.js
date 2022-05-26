c24de84e678061bb9c161aa9ceb181a6ff223fa1
milenegj <milene.jannetti@gmail.com>
2022-05-26 17:07:00 -0500

    SignUp Page OK

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,18 +6,48 @@ export default function LoginPage () {
     return(
         <Content>
             <img src={mainLogo} alt="main logo" />
-            <forms>
+            <form>
                 <input type="text" placeholder='email' />
                 <input type="text" placeholder='senha' />
                 <button type="submit">Entrar</button>
-            </forms>
+            </form>
             <Link to="/cadastro">Não tem uma conta? Cadastre-se!</Link>
         </Content>
     )
 }
 
 const Content = styled.div`
+margin-top:70px;
 display:flex;
 flex-direction:column;
-align-items:center
+align-items:center;
+padding:0 40px;
+box-sizing:border-box;
+
+form{
+    display:block;
+    margin:40px 0 30px 0;
+}
+input{
+    width:100%;
+    height:45px;
+    border:solid 1px #D4D4D4;
+    border-radius:5px;
+    font-family: 'Lexend Deca', sans-serif;
+    color:#DBDBDB;
+    font-size:20px;
+    margin-bottom: 5px;
+    padding:0 10px;
+    box-sizing:border-box;
+}
+button{
+    width:100%;
+    height:45px;
+    border:none;
+    border-radius:5px;
+    background-color:#52B6FF;
+    color:#FFFFFF;
+    font-size:20px;
+    font-family: 'Lexend Deca', sans-serif;
+}
 `
--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,25 +1,121 @@
-import styled from 'styled-components'
-import mainLogo from '../assets/images/Group 8.png'
-import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+import mainLogo from '../assets/images/Group 8.png';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-export default function SignUpPage () {
-    return(
-        <Content>
+import { ThreeDots } from  'react-loader-spinner'
+
+export default function SignUpPage() {
+    const navigate = useNavigate();
+    const [loading,setLoading] = useState("n")
+    const [newUserData, setNewUserData] = useState({
+        email: "",
+        name: "",
+        image: "",
+        password: ""
+    });
+
+    function sendNewUserData(e) {
+        e.preventDefault();
+        console.log(newUserData);
+        const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up";
+        const promise = axios.post(URL,newUserData);
+        setLoading("y");
+        promise.then(()=>navigate("/"));
+        promise.catch(handleError);
+    }
+
+    function handleError (error) {
+        alert(`${error.response.status} - ${error.response.data}`);
+        setLoading("n");
+    }
+
+    function showField(field) {
+        switch (field) {
+            case "email":
+                return newUserData.email;
+            case "senha":
+                return newUserData.password;
+            case "nome":
+                return newUserData.name;
+            case "foto":
+                return newUserData.image;
+            default:
+                return "";
+        }
+    }
+
+    function modifyField(e, field) {
+        switch (field) {
+            case "email":
+                setNewUserData({ ...newUserData, email: e.target.value })
+                break
+            case "senha":
+                setNewUserData({ ...newUserData, password: e.target.value })
+                break
+            case "nome":
+                setNewUserData({ ...newUserData, name: e.target.value })
+                break
+            case "foto":
+                setNewUserData({ ...newUserData, image: e.target.value })
+                break
+            default:
+                break;
+        }
+    }
+
+    return (
+        <Content loading={loading}>
             <img src={mainLogo} alt="main logo" />
-            <forms>
-                <input type="text" placeholder='email' />
-                <input type="text" placeholder='senha' />
-                <input type="text" placeholder='nome' />
-                <input type="text" placeholder='foto' />
-                <button type="submit">Cadastrar</button>
-            </forms>
+            <form onSubmit={sendNewUserData}>
+                <input type="email" placeholder='email' value={showField("email")} onChange={(e) => modifyField(e, "email")} required />
+                <input type="password" placeholder='senha' value={showField("senha")} onChange={(e) => modifyField(e, "senha")} required />
+                <input type="text" placeholder='nome' value={showField("nome")} onChange={(e) => modifyField(e, "nome")} required />
+                <input type="url" placeholder='foto' value={showField("foto")} onChange={(e) => modifyField(e, "foto")} required />
+                <button type="submit">{loading==="y"?<ThreeDots color="#FFFFFF" height={80} width={80}/>:"Cadastrar"}</button>
+            </form>
             <Link to="/">Já tem uma conta? Faça login!</Link>
         </Content>
     )
 }
 
 const Content = styled.div`
+margin-top:70px;
 display:flex;
 flex-direction:column;
-align-items:center
+align-items:center;
+padding:0 40px;
+box-sizing:border-box;
+
+form{
+    display:block;
+    margin:40px 0 30px 0;
+}
+input{
+    width:100%;
+    background-color:${({loading})=>loading==="y"?"#F2F2F2":"#FFFFFF"};
+    height:45px;
+    border:solid 1px #D4D4D4;
+    border-radius:5px;
+    font-family: 'Lexend Deca', sans-serif;
+    color:${({loading})=>loading==="y"?"#AFAFAF":"#DBDBDB"};
+    font-size:20px;
+    margin-bottom: 5px;
+    padding:0 10px;
+    box-sizing:border-box;
+}
+button{
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    width:100%;
+    height:45px;
+    border:none;
+    border-radius:5px;
+    background-color:${({loading})=>loading==="y"?"#75b6e6":"#52B6FF"};
+    color:#FFFFFF;
+    font-size:20px;
+    font-family: 'Lexend Deca', sans-serif;
+}
 `
